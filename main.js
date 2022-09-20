53d012f9a10b5df0af1c1eee7e7ec70bac4c0496
Nathan Flacher <nathan.flacher.pro@gmail.com>
2022-09-20 16:16:42 +0200

    second radio button

--- main.js
+++ main.js
@@ -8,17 +8,32 @@ document.getElementById('run').addEventListener("click", () => {
     let getJsonData = document.querySelector('input#get-json-data').checked;
     let getHtmlMovies = document.querySelector('input#get-html-movies').checked;
     let getJsonMovies = document.querySelector('input#get-json-movies').checked;
+    let target = document.querySelector('section#target')
     
     if (getHtmlData) {
         fetch("http://127.0.0.1:5500/ajax/data/1-get-html-article.html")
         .then(response => response.text())
         .then(datas => {
-            document.querySelector('section#target').innerHTML = datas;
+            target.innerHTML = datas;
         })
         .catch(error => alert("error : " + error));
     }
     if (getJsonData) {
-        console.log("getJsonData");
+        fetch("http://127.0.0.1:5500/ajax/data/2-get-contacts-list.json")
+        .then(response => response.json())
+        .then(datas => {
+            console.log(datas);
+            target.innerHTML = ''
+            let UL = document.createElement("ul");
+            for (const data of datas) {
+                const LI = document.createElement("li");
+                const liText = document.createTextNode(`${data.firstName} - ${data.phone}`);
+                LI.appendChild(liText);
+                UL.appendChild(LI);
+            }
+            target.appendChild(UL); 
+        })
+        .catch(error => alert("error : " + error));
     }
     if (getHtmlMovies) {
         console.log("getHtmlMovies");
